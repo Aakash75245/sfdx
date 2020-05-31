8fd5d1251d765b2b4316517a1f0b31acbddf461c
unknown <=>
2020-05-31 13:34:24 +0530

    Pushing changes through Plugins

--- aakashCli_1/src/commands/Aakash/File/read.ts
+++ aakashCli_1/src/commands/Aakash/File/read.ts
@@ -1,9 +1,9 @@
+// Import All Required Modules
 import { flags, SfdxCommand } from '@salesforce/command';
 import { Messages } from '@salesforce/core';
 import { AnyJson } from '@salesforce/ts-types';
 import * as fs from 'fs';
 import { join } from 'path';
-import { get } from 'lodash';
 import { promisify } from 'util';
 const readFile = promisify(fs.readFile)
 // Initialize Messages with the current plugin directory
@@ -34,9 +34,9 @@ export default class read extends SfdxCommand {
   };
   public async run(): Promise<AnyJson> {
     let jsonPath = `${join(process.cwd(), 'details.json')}`;
-    let json: any = await readFile(jsonPath, 'utf-8')
-    //this.ux.log('Data in JSON File is ' + json);
-    this.ux.log('plugins.prefix value is : '+get(json, 'plugins.prefix', this.flags.prefix));
+    let json: any = await readFile(jsonPath, 'utf-8');
+    this.ux.log('Data in JSON File is ' + JSON.parse(json).myName);
+    //this.ux.log('plugins.prefix value is : '+get(json,''));
     // Return an object to be displayed with --json
     return { Data : json };
   }
